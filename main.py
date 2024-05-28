98e37be25858d759367f089d59e7a41d45db61d8
Shienae Cedro <shienae.cedro@gmail.com>
2024-05-28 17:55:46 +0800

    fixed main.py

--- main.py
+++ main.py
@@ -15,23 +15,18 @@ from torchvision.models import vgg19, VGG19_Weights
 from imgurpython import ImgurClient
 
 
-
 customtkinter.set_appearance_mode("light")
 customtkinter.set_default_color_theme("theme/dark-blue.json")
-
 
 app = customtkinter.CTk(fg_color="White")
 app.geometry("702x560")
 app.title("Photo Art Style Transfer")
 
-
 clicked = False
 styleinput = False
 contentinput = False
 
-
 neuralthread1 = lambda: threading.Thread(target=neural_style_transfer)
-
 
 facebookthread = lambda: threading.Thread(target=share_on_facebook(imgur_link))
 copythread = lambda: threading.Thread(target=copy_imgur_link(imgur_link))
@@ -66,14 +61,17 @@ def neuralthread():
         neural_thread = neuralthread1()  
         neural_thread.start()
         if neural_thread.is_alive():
-            button3.configure(app, text="Cancel", command=cancel)
+            button3.configure(app, text="cancel_function", command=cancel_function)
             horizontal.grid_remove()
             horizontal_label.grid_remove()
             horizontal_label2.grid_remove()
             progress.grid(row=5, column=2, columnspan=2, pady=20)
             progress_percentage_label.grid(row=4, column=2, columnspan=2)
 
-def cancel():
+def slider_function(value):
+    text_var.set(f'{int(horizontal.get())}')
+
+def cancel_function():
     global clicked
     clicked = True
     button3.configure(text="Start Styling", command=neuralthread)
@@ -81,9 +79,6 @@ def cancel():
     horizontal.grid(row=5, column=3)
     horizontal_label.grid(row=6, column=3)
     horizontal_label2.grid(row=4, column=3)
-
-def slider_event(value):
-    text_var.set(f'{int(horizontal.get())}')
 
 def create_top():
     global toplevel
@@ -133,34 +128,14 @@ def top_widgets():
     )
     button_image_4 = tk.PhotoImage(
     file=("images/button_3.png"))
-    back_button = customtkinter.CTkButton(toplevel,text="",command=back_event,image=button_image_4,border_width=0,fg_color="transparent",width=17
+    back_button = customtkinter.CTkButton(toplevel,text="",command=back_function,image=button_image_4,border_width=0,fg_color="transparent",width=17
     )
     back_button.place(
         x=422.0,
         y=243.0
     )
 
-def back_event():
-    toplevel.withdraw()
-    app.deiconify()
-
-def style_event():
-    global stylepath, styleinput
-    stylepath = filedialog.askopenfilename(
-        initialdir="/pictures", title="Select Image", 
-        filetypes=(("jpg images", "*.jpg"), ("all files", "*.*"))
-    )
-    if stylepath:
-        styleinput = True
-        try:
-            image = Image.open(stylepath)
-            image_to_display2 = customtkinter.CTkImage(image, size=(300, 300))
-            image_label2.configure(image=image_to_display2)
-            image_label2.image = image_to_display2
-        except (FileNotFoundError, PermissionError) as e:
-            print(f"Error opening image: {e}")
-
-def content_event():
+def content_function():
     global contentinput, contentpath
     contentpath = filedialog.askopenfilename(
         initialdir="/pictures", title="Select Image", 
@@ -173,6 +148,22 @@ def content_event():
             image_to_display1 = customtkinter.CTkImage(image, size=(300, 300))
             image_label1.configure(image=image_to_display1)
             image_label1.image = image_to_display1
+        except (FileNotFoundError, PermissionError) as e:
+            print(f"Error opening image: {e}")
+
+def style_function():
+    global stylepath, styleinput
+    stylepath = filedialog.askopenfilename(
+        initialdir="/pictures", title="Select Image", 
+        filetypes=(("jpg images", "*.jpg"), ("all files", "*.*"))
+    )
+    if stylepath:
+        styleinput = True
+        try:
+            image = Image.open(stylepath)
+            image_to_display2 = customtkinter.CTkImage(image, size=(300, 300))
+            image_label2.configure(image=image_to_display2)
+            image_label2.image = image_to_display2
         except (FileNotFoundError, PermissionError) as e:
             print(f"Error opening image: {e}")
 
@@ -334,7 +325,7 @@ def neural_style_transfer():
                 global progress_value
                 progress_value = run[0] / numsteps
                 if progress_value > 1:
-                    progress_value = 1  # Cap the progress at 100%
+                    progress_value = 1
                 progress.set(progress_value-0.03)
                 progress_percentage_label.configure(text=f"{int(progress_value * 100)}%")
 
@@ -383,6 +374,10 @@ def neural_style_transfer():
         app.wait_window(toplevel)
         app.deiconify()
 
+def back_function():
+    toplevel.withdraw()
+    app.deiconify()
+
 def upload_image_to_imgur(displayimg):
     client_id = '9a3ec0ee4b97b64'
     client_secret = '4fba82309dd56809b0eaa54bd826c9f741a3033e'
@@ -390,22 +385,9 @@ def upload_image_to_imgur(displayimg):
     image = client.upload_from_path(displayimg, anon=True)
     return image['link']
 
-def share_on_facebook(imgur_link):
-    facebook_url = f"https://www.facebook.com/sharer/sharer.php?u={imgur_link}"
-    webbrowser.open(facebook_url, new=1)
-
-def copy_imgur_link(imgur_link):
-    """Copies the Imgur link to the clipboard and displays a confirmation message."""
-    try:
-        app.clipboard_append(imgur_link)
-        messagebox.showinfo("Success!", "Imgur link copied to clipboard.")
-    except tk.TclError:
-        messagebox.showerror("Error", "Failed to copy link. Please check clipboard permissions.")
-
 def on_download_button_click():
     img = Image.open(displayimg)
     save_image(img)
-
 
 def save_image(img):
     if img is None:
@@ -422,8 +404,19 @@ def save_image(img):
         except Exception as e:
             messagebox.showerror("Error", f"Could not save image: {e}")
 
+def share_on_facebook(imgur_link):
+    facebook_url = f"https://www.facebook.com/sharer/sharer.php?u={imgur_link}"
+    webbrowser.open(facebook_url, new=1)
 
-button1 = customtkinter.CTkButton(app, text="Select Content Image", command=content_event)
+def copy_imgur_link(imgur_link):
+    try:
+        app.clipboard_append(imgur_link)
+        messagebox.showinfo("Success!", "Imgur link copied to clipboard.")
+    except tk.TclError:
+        messagebox.showerror("Error", "Failed to copy link.")
+
+
+button1 = customtkinter.CTkButton(app, text="Select Content Image", command=content_function)
 button1.grid(row=3, column=2, padx=5, pady=5)
 
 placeholder_image1 = Image.open("images/imgholder.jpg")
@@ -431,7 +424,7 @@ placeholder_image_to_display1 = customtkinter.CTkImage(placeholder_image1, size=
 image_label1 = customtkinter.CTkLabel(app, image=placeholder_image_to_display1, text="", fg_color="White", width=310, height=310)
 image_label1.grid(row=2, column=2, padx=20, pady=10)
 
-button2 = customtkinter.CTkButton(app, text="Select Style Image", command=style_event)
+button2 = customtkinter.CTkButton(app, text="Select Style Image", command=style_function)
 button2.grid(row=3, column=3, padx=5, pady=5) 
 
 placeholder_image2 = Image.open("images/imgholder.jpg")
@@ -439,7 +432,7 @@ placeholder_image_to_display2 = customtkinter.CTkImage(placeholder_image2, size=
 image_label2 = customtkinter.CTkLabel(app, image=placeholder_image_to_display2, text="", fg_color="White", width=310, height=310)
 image_label2.grid(row=2, column=3, padx=20, pady=10)
 
-horizontal = customtkinter.CTkSlider(app, from_=1, to=1000, command=slider_event)
+horizontal = customtkinter.CTkSlider(app, from_=1, to=1000, command=slider_function)
 horizontal.grid(row=5, column=2, columnspan=2, pady=10)
 text_var = tk.StringVar(value="")
 
